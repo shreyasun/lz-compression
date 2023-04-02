0158aca55a66e0708f6d98387805720ac1867fe2
shreyasun <shreyasundar27@gmail.com>
2023-04-02 15:18:29 -0700

    codes

---
+++ code.h
@@ -0,0 +1,13 @@
+// PROVIDED HEADER FILE FOR CSE 13S
+
+#ifndef __CODE_H__
+#define __CODE_H__
+
+#include <inttypes.h>
+
+#define STOP_CODE  0
+#define EMPTY_CODE 1
+#define START_CODE 2
+#define MAX_CODE   UINT16_MAX
+
+#endif
